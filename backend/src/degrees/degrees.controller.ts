ba22b30ae47aa6f2662021650d7c1a1962e08be1
rsheynin <rsheynin@gmail.com>
2022-01-06 21:20:26 +0200

    degre

--- backend/src/degrees/degrees.controller.ts
+++ backend/src/degrees/degrees.controller.ts
@@ -4,14 +4,9 @@ import { DegreesService } from './degrees.service';
 @Controller('degrees')
 export class DegreesController {
   constructor(private readonly degreesService: DegreesService) {}
-  degrees: string[] = [
-    "Computer Scinece degree",
-    "Philosophy degree",
-    "Psychology degree",
-    "Ecomomics degree",
-];
+ 
   @Get()
   findAll(): string[] {
-    return this.degrees;
+    return this.degreesService.getAll();
   }
 }
--- backend/src/degrees/degrees.service.ts
+++ backend/src/degrees/degrees.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class DegreesService {}
+export class DegreesService {
+    private degrees: string[] = [
+        "Computer Scinece degree",
+        "Philosophy degree",
+        "Psychology degree",
+        "Ecomomics degree",
+    ];
+    getAll(){
+        return this.degrees;
+    }
+}
