8d56c9c1adf894c2f13b069cf4b3a55b2ee8ce5a
rsheynin <rsheynin@gmail.com>
2022-05-31 21:46:07 +0300

    remove node backend

--- backend/src/degrees/degrees.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DegreesService } from './degrees.service';
-import { DegreesController } from './degrees.controller';
-
-@Module({
-  controllers: [DegreesController],
-  providers: [DegreesService]
-})
-export class DegreesModule {}
